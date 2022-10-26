b0fedcc3deb1dfed9bc10b9150ff31bccaa5497c
EnderStar101 <mcm7565@gmail.com>
2022-10-26 13:11:39 -0400

    Finished Unit 7 Assignment

--- Unit_7/photo-gallery/src/pages/Tab2.tsx
+++ Unit_7/photo-gallery/src/pages/Tab2.tsx
@@ -6,7 +6,7 @@ import './Tab2.css';
 
 const Tab2: React.FC = () => {
 
-  const { takePhoto } = usePhotoGallery();
+  const { photos, takePhoto } = usePhotoGallery();
   return (
     <IonPage>
       <IonHeader>
@@ -20,6 +20,15 @@ const Tab2: React.FC = () => {
             <IonIcon icon = {camera}></IonIcon>
           </IonFabButton>
         </IonFab>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size = "6" key = {index}>
+                <IonImg src = {photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>   
+        </IonGrid>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Photo Gallery</IonTitle>
