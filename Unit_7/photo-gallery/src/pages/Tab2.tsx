b01dd2a73b300756ae5ab6339be5fe7ad41dcb83
EnderStar101 <mcm7565@gmail.com>
2022-10-26 12:23:47 -0400

    Unit 7 Photo Gallery partway through

---
+++ Unit_7/photo-gallery/src/pages/Tab2.tsx
@@ -0,0 +1,34 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg, IonActionSheet } from '@ionic/react';
+import { camera, trash, close } from 'ionicons/icons'
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab2.css';
+
+const Tab2: React.FC = () => {
+
+  const { takePhoto } = usePhotoGallery();
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab 2</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonFab vertical = "bottom" horizontal = "center" slot = "fixed">
+          <IonFabButton onClick = {() => takePhoto()}>
+            <IonIcon icon = {camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Photo Gallery</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name="Tab 2 page" />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
