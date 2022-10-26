a54ed56044f3ec165920dde3a7d00e2cf8582c04
EnderStar101 <mcm7565@gmail.com>
2022-10-26 13:13:47 -0400

    Completed Unit 7 Assignment

--- Unit_7/photo-gallery/src/hooks/usePhotoGallery.ts
+++ Unit_7/photo-gallery/src/hooks/usePhotoGallery.ts
@@ -8,16 +8,34 @@ import { Capacitor } from '@capacitor/core';
 
 export function usePhotoGallery()
 {
+    const [photos, setPhotos] = useState<UserPhoto[]>([]);
+
     const takePhoto = async () => {
         const photo = await Camera.getPhoto({
             resultType: CameraResultType.Uri,
             source: CameraSource.Camera,
             quality: 100,
         });
+
+        const fileName = new Date().getTime() + '.jpeg';
+        const newPhotos = [
+            {
+                filepath: fileName,
+                webviewPath: photo.webPath,
+            },
+            ...photos,
+        ];
+        setPhotos(newPhotos);
     };
 
-    return 
-    {
+    return {
+        photos,
         takePhoto,
     };
+}
+
+export interface UserPhoto 
+{
+    filepath: string;
+    webviewPath?: string;
 }
